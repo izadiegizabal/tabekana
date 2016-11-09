9a398b3bf7154a317d05e0fd7cbbbda00a6e4964
Izadi Egizabal <64izadi@gmail.com>
2016-11-09 02:43:15 +0100

    Initial commit with MainMenu's basic buttons

---
+++ Tabekana/Assets/Scripts/RandomChar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RandomSprite : MonoBehaviour {
+
+	private string chr = "あいうえおかきくけこがぎぐげごさしすせそざじずぜぞたちつてとだぢづでどなにぬねのはひふへほばびぶべぼぱtぴぷぺぽまみむめもやゆよらりるれろわをん" +
+		"アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲンガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポ";
+	private char[] characters;
+	private Text actualChar;
+
+	public Font customFont;
+
+	// Use this for initialization
+	void Start () {
+		if(chr != null){
+			characters = chr.ToCharArray();
+			GetComponent<Text>().text = characters[Random.Range (0, characters.Length)];
+			GetComponent<Text> ().font = customFont;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
