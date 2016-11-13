ad38757c6a61a93af46e2e9ce1b98493d6b68d80
Izadi Egizabal <64izadi@gmail.com>
2016-11-13 17:15:04 +0100

    Started the Sushi Staging + rice explosions

---
+++ Tabekana/Assets/Scripts/Explode.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explode : MonoBehaviour {
+
+	public Rice rice;
+	public int totalParts = 10;
+
+
+	private Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D target){
+		if (target.gameObject.tag == "Shuriken") {
+			animator.SetInteger ("AnimState", 1);
+		}
+
+	}
+
+	void OnCollisionEnter2D(Collision2D target){
+		if (target.gameObject.tag == "Shuriken") {
+			animator.SetInteger ("AnimState", 1);
+		}
+
+	}
+
+
+	public void Destroy(){
+	
+		Destroy (gameObject);
+
+		var t = transform;
+
+		for (int i = 0; i < totalParts; i++) {
+			Rice clone = Instantiate(rice, t.position, Quaternion.identity) as Rice;
+			clone.GetComponent<Rigidbody2D>().AddForce(Vector3.right * (Random.Range (-100, 100)));
+			clone.GetComponent<Rigidbody2D>().AddForce(Vector3.up * Random.Range(75, 300));
+		}
+
+	}
+}
---
+++ Tabekana/Assets/Scripts/RandomSushi.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSushi : MonoBehaviour {
+
+	public Sprite[] sprites;
+	public string resourceName;
+	public int level = 1;
+
+	private  int currentSprite;
+	private int untilWhat;
+	// Use this for initialization
+	void Start () {
+		if(resourceName != null){
+			sprites = Resources.LoadAll<Sprite> (resourceName);
+
+			switch(level){
+			case 1:
+				untilWhat = ;
+				break;
+			case 2:
+			}
+
+
+
+
+
+
+
+
+			currentSprite = Random.Range (0, untilWhat);
+
+			GetComponent<SpriteRenderer> ().sprite = sprites[currentSprite];
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
