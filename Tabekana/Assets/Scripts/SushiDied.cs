166f68a0d256aa90abc8d0a3e02c5ac28f004b25
ClaudiaCambra <claudiacambram@gmail.com>
2016-11-17 02:03:52 +0100

    created the scene Level Over  and the scrip SushiDied

---
+++ Tabekana/Assets/Scripts/SushiDied.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SushiDied : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter2D(Collider2D target){
+		if (target.gameObject.tag == "Shusi") {
+
+			loadLevel(string LevelOver);
+		}
+
+	}
+}
