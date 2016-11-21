9ee68713a71081fa0ac4531fcdca24d4ed59c91d
Izadi Egizabal <64izadi@gmail.com>
2016-11-21 23:52:24 +0100

    Keyboard shows input in the correct place

--- Tabekana/Assets/Scripts/GlobalVariables.cs
+++ Tabekana/Assets/Scripts/GlobalVariables.cs
@@ -3,23 +3,6 @@ using System.Collections;
 
 public static class GlobalVariables : object {
 		
-	public static string[] array3Characters = new string [3] {"_", "_", "_"};
+	public static string[] array3Characters = new string [3] {"_", "", ""};
 
-	//public GlobalVariables(){
-		
-		//array3Characters [0] = "_";
-		//array3Characters [1] = "_";
-		//array3Characters [2] = "_";
-	//}
-
-
-	// Use this for initialization
-/*	void Start () {
-		//array3Characters = new string[] { "_", "_", "_" };
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}*/
 }
