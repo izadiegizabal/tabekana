798d04df9fd0393100742035354b2d1d35c30fa4
Izadi Egizabal <64izadi@gmail.com>
2016-11-23 11:37:15 +0100

    Revert "Revert "Keyboard shows input in the correct place""
    This reverts commit 8a2753c7fa1c7911378c922a0b0f040459c808fd.

--- Tabekana/Assets/Scripts/KeyBoard.cs
+++ Tabekana/Assets/Scripts/KeyBoard.cs
@@ -6,12 +6,10 @@ public class KeyBoard : MonoBehaviour {
 
 	string letra;
 
-	//public static bool leerClic (char datosDelClic){   <---- COMENTADO
 	public void leerClic (string datosDelClic){
 		// Transformamos el clic en una letra
 		letra = leeLetra(datosDelClic);
 		// La escribimos por pantalla
-		//return this.escribeLetra(letra);               <---- COMENTADO
 		escribeLetra(letra);
 
 	}
@@ -24,61 +22,61 @@ public class KeyBoard : MonoBehaviour {
 		switch (datosDelClic) {
 			// Primera linea
 			case "Q":
-				letra = "Q"; break;
+				letra = "q"; break;
 			case "W":
-				letra = "W"; break;
+				letra = "w"; break;
 			case "E":
-				letra = "E"; break;
+				letra = "e"; break;
 			case "R":
-				letra = "R"; break;
+				letra = "r"; break;
 			case "T":
-				letra = "T"; break;
+				letra = "t"; break;
 			case "Y":
-				letra = "Y"; break;
+				letra = "y"; break;
 			case "U":
-				letra = "U"; break;
+				letra = "u"; break;
 			case "I":
-				letra = "I"; break;
+				letra = "i"; break;
 			case "O":
-				letra = "O"; break;
+				letra = "o"; break;
 			case "P":
-				letra = "P"; break;
+				letra = "p"; break;
 
 			// Segunda linea
 			case "A":
-				letra = "A"; break;
+				letra = "a"; break;
 			case "S":
-				letra = "S"; break;
+				letra = "s"; break;
 			case "D":
-				letra = "D"; break;
+				letra = "d"; break;
 			case "F":
-				letra = "F"; break;
+				letra = "f"; break;
 			case "G":
-				letra = "G"; break;
+				letra = "g"; break;
 			case "H":
-				letra = "H"; break;
+				letra = "h"; break;
 			case "J":
-				letra = "J"; break;
+				letra = "j"; break;
 			case "K":
-				letra = "K"; break;
+				letra = "k"; break;
 			case "L":
-				letra = "L"; break;
+				letra = "l"; break;
 
 			// Tercera linea
 			case "Z":
-				letra = "Z"; break;
+				letra = "z"; break;
 			case "X":
-				letra = "X"; break;
+				letra = "x"; break;
 			case "C":
-				letra = "C"; break;
+				letra = "c"; break;
 			case "V":
-				letra = "V"; break;
+				letra = "v"; break;
 			case "B":
-				letra = "B"; break;
+				letra = "b"; break;
 			case "N":
-				letra = "N"; break;
+				letra = "n"; break;
 			case "M":
-				letra = "M"; break;
+				letra = "m"; break;
 		}
 		return letra;
 	}
@@ -94,32 +92,14 @@ public class KeyBoard : MonoBehaviour {
 
 		if (GlobalVariables.array3Characters [0] == "_") {
 			GlobalVariables.array3Characters [0] = letra;
+			GlobalVariables.array3Characters [1] = "_";
 		} 
 		else if (GlobalVariables.array3Characters [1] == "_") {
 			GlobalVariables.array3Characters [1] = letra;
+			GlobalVariables.array3Characters [2] = "_";
 		} 
 		else if (GlobalVariables.array3Characters [2] == "_") {
 			GlobalVariables.array3Characters [2] = letra;
 		}
-//		for (int i = 0; i < GlobalVariables.array3Characters.Length; i++) {
-//			print (GlobalVariables.array3Characters[i]);                     // <---- MUESTRA EL ARRAY EN LA CONSOLA   
-//		}
-	}
-
-	void OnGUI () {
-	//	GUILayout.Label(letra);                                               // <---- MUESTRA LA LETRA POR PANTALLA   
-	//	for (int i = 0; i < GlobalVariables.array3Characters.Length; i++) {
-	//		GUILayout.Label (GlobalVariables.array3Characters [i]);           // <---- MUESTRA EL ARRAY POR PANTALLA POR POSICIONES Y EN VERTICAL 
-	//	}
-		if (GlobalVariables.array3Characters [1] != "_") {
-			GUILayout.Label (string.Join ("", GlobalVariables.array3Characters)); // <---- MUESTRA EL ARRAY POR PANTALLA BIEN!   
-		} 
-		else {
-			//GlobalVariables.array3Characters [3] = "";   --> ERROR! 
-			GlobalVariables.array3Characters = new string [2] {GlobalVariables.array3Characters[0], GlobalVariables.array3Characters[1]};	// Lo convierte en un array de dos posiciones
-			GUILayout.Label (string.Join ("", GlobalVariables.array3Characters)); // <---- MUESTRA EL ARRAY POR PANTALLA BIEN! 
-			GlobalVariables.array3Characters = new string [3] {GlobalVariables.array3Characters[0], GlobalVariables.array3Characters[1], "_"};	// Lo deja con 3 posiciones para la siguiente vez
-		}
-
 	}
 }
