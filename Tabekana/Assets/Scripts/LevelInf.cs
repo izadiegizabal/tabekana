d9bda7a5fd2715d8512158b0f6b7b00ff2bb84c2
ClaudiaCambra <claudiacambram@gmail.com>
2016-12-02 18:51:55 +0100

    Sushi change whit code

---
+++ Tabekana/Assets/Scripts/LevelInf.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+
+
+//[System.Serializable]
+//public class AnimationSettings{//Settings for how your UI change during runtime
+//	public enum OpenStyle{WhidtToHeight, HeightToWidth,HeightAndWidht}
+
+//}
+
+public class UISettings{
+	//public Image textBox;//The box tha will contain the text
+	//public Text text;//the message
+	//añadir tamaños
+
+	//public bool openning=false;
+
+}
+	
+public class LevelInf : MonoBehaviour {
+	public Sprite uno;
+	public Sprite suno;
+
+	 
+
+
+
+	//public LevelInf codigo;
+	//public GameObject nivel; 
+	// Use this for initialization
+	void Start () {
+		
+		suno=gameObject.GetComponent<LevelInf>().uno;
+
+
+
+		//nivel = GameObject.Find ("sushis");
+		//codigo = gameObject.GetComponent<LevelInf>().uno;
+
+	}
+	public Sprite Setuno(){
+		return uno;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
