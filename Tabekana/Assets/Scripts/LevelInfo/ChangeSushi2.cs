32a58f72e3fdfc8c1df5bb80e852ee2d152752f9
ClaudiaCambra <claudiacambram@gmail.com>
2016-12-05 14:13:12 +0100

    ChangeSushi 1,2,3,4 and 5 from LevelInfo2 finished

--- Tabekana/Assets/Scripts/LevelInfo/ChangeSushi2.cs
+++ Tabekana/Assets/Scripts/LevelInfo/ChangeSushi2.cs
@@ -1,17 +1,138 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System;
 
 public class ChangeSushi2: MonoBehaviour {
 	public LevelInf codigo;
 	public GameObject sushis;
 	public Image imagen;
+
+
 	// Use this for initialization
 	void Start () {
-		sushis = GameObject.Find ("sushis");
-		codigo=sushis.GetComponent<LevelInf>();
-		imagen=gameObject.GetComponent<Image>();
-		imagen.sprite = codigo.b;
+		String value = "h 15";
+		//en esta varibale cambiar por el metodo que lanza alba 
+
+		Char delimiter = ' ';
+		String[] substrings = value.Split(delimiter);
+		string a = substrings [0];
+		string b = substrings [1];
+		char u = char.Parse (a);
+		int d = int.Parse (b);
+
+		if (u.Equals('h')) {
+			//Hiragana
+			if (d==1){
+				//Lesson 1
+				//m_tittletex="Lesson 1";
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.b;
+
+			}
+			if (d==2){
+				//Lesson 2
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.g;
+			}
+			if (d==3) {
+				//Lesson 3
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.l;
+			}
+			if (d==4) {
+				//Lesson 4
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.q;
+			}
+			if (d==5) {
+				//Lesson 5
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.v;
+			}
+			if (d==6) {
+				//Lesson 6
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.aa;
+			}
+			if (d==7) {
+				//Lesson 7
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.fa;
+			}
+			if (d==8) {
+				//Lesson 8
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.ka;
+			}
+			if (d==9) {
+				//Lesson 9
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.na;
+			}
+			if (d==10) {
+				//Lesson 10
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.sa;
+			}
+			if (d==11) {
+				//Lesson 11
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.va;
+			}
+			if (d==12) {
+				//Lesson 12
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.ab;
+			}
+			if (d==13) {
+				//Lesson 13
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.fb;
+			}
+			if (d==14) {
+				//Lesson 14
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.kb;
+			}
+			if (d==15) {
+				//Lesson 15
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.pb;
+			}
+
+		}
 	}
 
 	// Update is called once per frame
--- Tabekana/Assets/Scripts/LevelInfo/ChangeSushi3.cs
+++ Tabekana/Assets/Scripts/LevelInfo/ChangeSushi3.cs
@@ -1,17 +1,138 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System;
 
 public class ChangeSushi3: MonoBehaviour {
 	public LevelInf codigo;
 	public GameObject sushis;
 	public Image imagen;
+
+
 	// Use this for initialization
 	void Start () {
-		sushis = GameObject.Find ("sushis");
-		codigo=sushis.GetComponent<LevelInf>();
-		imagen=gameObject.GetComponent<Image>();
-		imagen.sprite = codigo.c;
+		String value = "h 15";
+		//en esta varibale cambiar por el metodo que lanza alba 
+
+		Char delimiter = ' ';
+		String[] substrings = value.Split(delimiter);
+		string a = substrings [0];
+		string b = substrings [1];
+		char u = char.Parse (a);
+		int d = int.Parse (b);
+
+		if (u.Equals('h')) {
+			//Hiragana
+			if (d==1){
+				//Lesson 1
+				//m_tittletex="Lesson 1";
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.c;
+
+			}
+			if (d==2){
+				//Lesson 2
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.h;
+			}
+			if (d==3) {
+				//Lesson 3
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.m;
+			}
+			if (d==4) {
+				//Lesson 4
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.r;
+			}
+			if (d==5) {
+				//Lesson 5
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.w;
+			}
+			if (d==6) {
+				//Lesson 6
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.ba;
+			}
+			if (d==7) {
+				//Lesson 7
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.ga;
+			}
+			if (d==8) {
+				//Lesson 8
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.la;
+			}
+			if (d==9) {
+				//Lesson 9
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.oa;
+			}
+			if (d==10) {
+				//Lesson 10
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.ta;
+			}
+			if (d==11) {
+				//Lesson 11
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.wa;
+			}
+			if (d==12) {
+				//Lesson 12
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.bb;
+			}
+			if (d==13) {
+				//Lesson 13
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.gb;
+			}
+			if (d==14) {
+				//Lesson 14
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.lb;
+			}
+			if (d==15) {
+				//Lesson 15
+				sushis = GameObject.Find ("sushis");
+				codigo=sushis.GetComponent<LevelInf>();
+				imagen=gameObject.GetComponent<Image>();
+				imagen.sprite = codigo.qb;
+			}
+
+		}
 	}
 
 	// Update is called once per frame
