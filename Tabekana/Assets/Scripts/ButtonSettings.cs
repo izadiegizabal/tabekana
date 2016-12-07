291b4f7904cb7e390e8d8afd348bc2e6e4bdf7a1
Albagaller <alba.g.aller@gmail.com>
2016-12-07 20:09:31 +0100

    Added level lock/unlock  system

---
+++ Tabekana/Assets/Scripts/ButtonSettings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonSettings : MonoBehaviour {
+
+    public static int releasedLevelStatic = GlobalVariables.levelUnlockHira;
+    public int releasedLevel;
+    public string nextLevel;
+
+
+    void Awake() {
+        if (GlobalVariables.actGameLvl.Split(new[] { " " }, System.StringSplitOptions.None)[0] == "h")
+        {
+            GlobalVariables.levelUnlockHira++;
+        }else{
+            GlobalVariables.levelUnlockKata++;
+        }
+       
+    }
+
+
+    public void ButtonMenu()
+    {
+        SceneManager.LoadScene("GameLevelChooser");
+    }
+}
---
+++ Tabekana/Assets/Scripts/LevelUnlockKatakana.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine.SceneManagement;using UnityEngine.UI;using UnityEngine;public class LevelUnlockKatakana : MonoBehaviour
+{    public int level;    public Image candado;    public Text texto;    private string levelString;
+
+    // Use this for initialization
+    void Start()
+    {
+     
+        if (GlobalVariables.levelUnlockKata >= level)        {            LevelUnlocked();        }
+        else        {            LevelLocked();        }
+    }
+
+    void LevelLocked()
+    {        GetComponent<Button>().interactable = false;        candado.enabled = true;        texto.enabled = false;    }    void LevelUnlocked()
+    {        GetComponent<Button>().interactable = true;        candado.enabled = false;        texto.enabled = true;    }
+
+}
