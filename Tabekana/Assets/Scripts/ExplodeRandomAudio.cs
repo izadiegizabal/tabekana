a05eb624d4e7c85fc44736e8ddda62555f60d3b6
Izadi Egizabal <64izadi@gmail.com>
2016-12-18 01:08:25 +0100

    AudioFX for sushi explosion and shuriken launch

---
+++ Tabekana/Assets/Scripts/ExplodeRandomAudio.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeRandomAudio : MonoBehaviour {
+
+	public AudioClip[] explosionClips;
+
+	public void RandomExplosionSound(){
+		GetComponent<AudioSource>().clip = explosionClips[Random.Range(0,explosionClips.Length)];
+		GetComponent<AudioSource>().Play();
+	}
+}
---
+++ Tabekana/Assets/Scripts/ShurikenSoundEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShurikenSoundEffect : MonoBehaviour {
+
+	public AudioClip[] movementSounds;
+
+	public void Start(){
+		GetComponent<AudioSource>().clip = movementSounds[Random.Range(0,movementSounds.Length)];
+		GetComponent<AudioSource>().Play();
+	}
+}
