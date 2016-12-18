8ae850235f8aabd364e1db20ee5a6ff23e6020f9
Albagaller <alba.g.aller@gmail.com>
2016-12-19 00:26:55 +0100

    Added Lose and Win infinite mode

---
+++ Tabekana/Assets/Scripts/ButtonWinInfinite.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonWinInfinite : MonoBehaviour
+{
+
+    //public static int releasedLevelStatic = GlobalVariables.levelUnlockHira;
+
+    void Awake()
+    {
+        //GlobalVariables.score = 0;//si no va, borra esto
+        GlobalVariables.array3Characters = new string[3] { "_", "", "" };
+        GlobalVariables.inputArrayChanged = false;
+        GlobalVariables.spawnedSushi.Clear();
+        GlobalVariables.launchPermission = false;
+        GlobalVariables.destroyedSushi = 0;
+
+    }
+
+
+    public void ButtonMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+
+    public void ButtonRepeat()
+    {
+        SceneManager.LoadScene("InfiniteMode", LoadSceneMode.Single);
+    }
+
+    public void ButtonLessons()
+    {
+            SceneManager.LoadScene("LessonChooser", LoadSceneMode.Single);
+    }
+
+}
---
+++ Tabekana/Assets/Scripts/InfiniteCleared.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class InfiniteCleared : MonoBehaviour {
+
+    public Text txtscore = null;
+    public Text txtmaxscore = null;
+
+    // Use this for initialization
+    void Start () {
+
+        txtscore.text = "" + GlobalVariables.score;
+        txtmaxscore.text = "" + GlobalVariables.maxScore;
+    }
+	
+}
